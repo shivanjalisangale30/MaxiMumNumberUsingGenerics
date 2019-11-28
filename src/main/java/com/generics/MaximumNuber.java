494f5b584ca830fc665e7528d9e33dd21accf261
shivanjalisangale30 <shivanjalisangale5456@gmail.com>
2019-11-28 17:11:19 +0530

    ADD: Find maximum Number from three float numbers are given

--- src/main/java/com/generics/MaximumNuber.java
+++ src/main/java/com/generics/MaximumNuber.java
@@ -10,4 +10,14 @@ public class MaximumNuber {
         }
         return maxNumber;
     }
+
+    public Float findMaximumNumber(Float firstNumber, Float secondNumber, Float thirdNumber) {
+        Float maxNumber = firstNumber;
+        if(secondNumber.compareTo(maxNumber)>0){
+             maxNumber = secondNumber;
+        }if(thirdNumber.compareTo(maxNumber)>0){
+             maxNumber = thirdNumber;
+        }
+        return maxNumber;
+    }
 }
--- src/test/java/com/generics/MaximumNumberTest.java
+++ src/test/java/com/generics/MaximumNumberTest.java
@@ -13,4 +13,14 @@ public class MaximumNumberTest {
         Integer result = maximumNuber.findMaximumNumber(firstNumber,secondNumber,thirdNumber);
         Assert.assertEquals(firstNumber,result);
     }
+
+    @Test
+    public void givenThreeNumber_whenFloat_maximumNumber() {
+        MaximumNuber maximumNuber = new MaximumNuber();
+        Float firstNumber = 55.5f;
+        Float secondNumber = 45.5f;
+        Float thirdNumber = 65.5f;
+        Float result = maximumNuber.findMaximumNumber(firstNumber, secondNumber, thirdNumber);
+        Assert.assertEquals(thirdNumber,result);
+    }
 }
